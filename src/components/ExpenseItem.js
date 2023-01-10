f9eba1ba5998e6b4fcd1a48b11151e79cd98e1e8
Makktu <johnmcna@gmail.com>
2023-01-10 20:10:58 +0000

    advancing...

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 2, 28);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.67;
     return (
         <div className="expense-item">
-            <div>28 March 2023</div>
+            <div>{expenseDate.toISOString().slice(0, 10)}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">£249.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">£{expenseAmount}</div>
             </div>
         </div>
     );
