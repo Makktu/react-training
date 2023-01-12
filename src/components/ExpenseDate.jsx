f497131633c8331fc25baf36c098e0801b3daf15
Makktu <johnmcna@gmail.com>
2023-01-12 03:17:24 +0000

    more work on this

---
+++ src/components/ExpenseDate.jsx
@@ -0,0 +1,14 @@
+function ExpenseDate(props) {
+    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+    const month = props.date.toLocaleString("en-US", { month: "long" });
+    const year = props.date.getFullYear();
+    return (
+        <div className="date_display">
+            <div>{day}</div>
+            <div>{month}</div>
+            <div>{year}</div>
+        </div>
+    );
+}
+
+export default ExpenseDate;
